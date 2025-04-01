5ca8f788f1a59737564f5005335d244dbb86b7f8
Ricardo Paulino <ricardo.paulino@byteon.com.br>
2025-04-01 17:36:08 -0300

    created entity account

---
+++ src/Domain/Entities/Account.cs
@@ -0,0 +1,8 @@
+namespace Domain.Entities;
+public sealed record Account
+{
+    public Guid IdAccount { get; set; }
+    public int Number { get; set; }
+    public string Name { get; set; }
+    public bool IsActive { get; set; }
+}
